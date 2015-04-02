2553291ae8920b0e628d48c4cec512dffd7cbf87
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-04-01 21:33:18 -0400

    Goodbye World
    I'll be back

---
+++ include/Dunjun/Common.h
@@ -0,0 +1,11 @@
+#ifndef DUNJUN_COMMON_H
+#define DUNJUN_COMMON_H
+
+namespace Dunjun 
+{
+	using Float32 = float;
+	using Float64 = double;
+
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Dunjun/Common.h>
+
+#include <iostream>
+
+int main(int argc, char** argv) 
+{
+	std::cout << "Goodbye World" << std::endl;
+
+	std::cin.ignore();
+
+	return 0;
+}
