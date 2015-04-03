d56c4d732d4d23d233bfff55a878f1b6612ab224
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-04-03 18:03:10 -0400

    add glfw lib and make a new window
    The window can be sized and have full screen version

--- include/Dunjun/Common.h
+++ include/Dunjun/Common.h
@@ -6,6 +6,9 @@ namespace Dunjun
 	using Float32 = float;
 	using Float64 = double;
 
+	#define GLOBAL static
+	#define INTERNAL static
+	#define LOCAL_PERSIST static
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,66 @@
 #include <Dunjun/Common.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 
+GLOBAL int g_windowWidth = 640;
+GLOBAL int g_windowHeight = 480;
+
 int main(int argc, char** argv) 
 {
-	std::cout << "Goodbye World" << std::endl;
+	GLFWwindow* window;
 
-	std::cin.ignore();
+	if (!glfwInit())
+	{
+		return EXIT_FAILURE;
+	}
 
-	return 0;
+	window = glfwCreateWindow(g_windowWidth, g_windowHeight, "Dunjun", nullptr, nullptr);
+
+	if (!window) 
+	{
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	bool full_screen = false;
+	bool running = true;
+	while (running)
+	{
+		glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		if (glfwWindowShouldClose(window))
+		{
+			running = false;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE))
+		{
+			running = false;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_F11))
+		{
+			full_screen = !full_screen;
+			GLFWwindow* newWindow;
+
+			if (full_screen)
+				newWindow = glfwCreateWindow(1600, 900, "Dunjun", glfwGetPrimaryMonitor(), window);
+			else
+				newWindow = glfwCreateWindow(g_windowWidth, g_windowHeight, "Dunjun", nullptr, window);
+
+			glfwDestroyWindow(window);
+			window = newWindow;
+			glfwMakeContextCurrent(window);
+		}
+	}
+
+	glfwTerminate();
+	return EXIT_SUCCESS;
 }
