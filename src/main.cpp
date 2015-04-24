57f56854aef4d07c99f52318b089a856718aa25f
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-04-23 20:52:29 -0400

    day 005

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 
 #include <gl/glew.h>
 #include <GLFW/glfw3.h>
-
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
 
 #include <iostream>
 #include <cmath>
@@ -53,10 +54,10 @@ int main(int argc, char** argv)
 
 	float vertices[] = {
 	//    x      y     r     g     b
-		+0.5f, +0.5f, 1.0f, 1.0f, 1.0f,  //Vertex 0
-		-0.5f, +0.5f, 0.0f, 0.0f, 1.0f,  //Vertex 1
-		+0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  //Vertex 2
-		-0.5f, -0.5f, 1.0f, 0.0f, 0.0f,  //Vertex 3
+		+0.5f, +0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,  //Vertex 0
+		-0.5f, +0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,  //Vertex 1
+		+0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  //Vertex 2
+		-0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,  //Vertex 3
 	};
 
 	GLuint vbo;
@@ -70,13 +71,39 @@ int main(int argc, char** argv)
 
 	shaderProgram.bindAttributeLocation(0, "vertPosition");
 	shaderProgram.bindAttributeLocation(1, "vertColor");
+	shaderProgram.bindAttributeLocation(2, "vertTexCoord");
 	shaderProgram.link();
 	shaderProgram.use();
+
+	GLuint tex;
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	unsigned char* image;
+	int width, height, comp;
+	image = stbi_load("data/textures/pig.jpg", &width, &height, &comp, 0);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+
+	glActiveTexture(GL_TEXTURE0);
+	shaderProgram.setUniform("uniTex", 0);
+
+	stbi_image_free(image);
 
 	bool full_screen = false;
 	bool running = true;
 	while (running)
 	{
+		{
+			int width, height;
+			glfwGetWindowSize(window, &width, &height);
+			glViewport(0, 0, width, height);
+		}
+
 		glClearColor(0.5f, 0.69f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
@@ -86,14 +113,17 @@ int main(int argc, char** argv)
 
 			glEnableVertexAttribArray(0);
 			glEnableVertexAttribArray(1);
+			glEnableVertexAttribArray(2);
 
-			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
-			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (const GLvoid*)(2 * sizeof(float)));
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (const GLvoid*)0);
+			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (const GLvoid*)(2 * sizeof(float)));
+			glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (const GLvoid*)(5 * sizeof(float)));
 
 			glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
 			glDisableVertexAttribArray(0);
 			glDisableVertexAttribArray(1);
+			glDisableVertexAttribArray(2);
 		}
 
 		glfwSwapBuffers(window);
